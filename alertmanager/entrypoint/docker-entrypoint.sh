b30982770da28b1ebb103d9a9131ff0a462509f8
currycan <ansandy@foxmail.com>
2018-12-18 18:16:21 +0800

    Update docker-entrypoint.sh

--- alertmanager/entrypoint/docker-entrypoint.sh
+++ alertmanager/entrypoint/docker-entrypoint.sh
@@ -1,61 +1,34 @@
+#!/bin/sh -e
 
-global:
-  resolve_timeout: 5m
-  # smtp_smarthost: 'localhost:25'
-  # smtp_smarthost: 'smtp.gmail.com:587'
-  # smtp_smarthost: 'smtp.163.com:25'
-  # smtp_smarthost:'smtp.exmail.qq.com:465'
-  smtp_smarthost: 'mail.fmsh.com.cn:25'
-  smtp_from: 'fmshiotbuild@fmsh.com.cn'
-  smtp_auth_username: 'fmshiotbuild'
-  smtp_auth_password: '<PASSWORD>'
-  smtp_require_tls: false
+set -exou pipefail
 
-route:
-  group_wait: 30s
-  group_interval: 5m
-  repeat_interval: '<REPEAT>'
-  group_by: [alertname]
-  routes:
-  - match:
-      severity: critical
-    receiver: webhook
-  - match:
-      severity: warning
-    receiver: wechat
-  - match:
-      severity: normal
-    receiver: e-mail
+if [ "${1:0:1}" = '-' ]; then
+    echo "Wait 5s for promethues up.. "
+    sleep 5
+    if [ -f /alertmanager.yml ];then
+        \cp -a /alertmanager.yml /etc/alertmanager/config/alertmanager.yml
+    else
+        cat /etc/alertmanager/config/alertmanager.yml | \
+            sed "s@<CORP_ID>@$CORP_ID@g" | \
+            sed "s@<PARTY>@$PARTY@g" | \
+            sed "s@<AGENT_ID>@$AGENT_ID@g" | \
+            sed "s@<AUTH_CODE>@$AUTH_CODE@g" | \
 
-templates:
-  - '/etc/alertmanager/template/*.tmpl'
+            sed "s!<EMAIL_USER>!$EMAIL_USER!g" | \
+            sed "s!<PASSWORD>!$PASSWORD!g" | \
 
-receivers:
-  - name: 'wechat'
-    wechat_configs:
-    - send_resolved: true
-      corp_id: '<CORP_ID>'
-      to_party: '<PARTY>'
-      agent_id: '<AGENT_ID>'
-      api_secret: '<AUTH_CODE>'
-      message: '{{ template "wechat.default.message" . }}'
+            sed "s@<SLACK_USER>@$SLACK_USER@g" | \
+            sed "s@<SLACK_CHANNEL>@$SLACK_CHANNEL@g" | \
+            sed "s@<SLACK_URL>@$SLACK_URL@g" | \
 
-  - name: 'e-mail'
-    email_configs:
-    - send_resolved: true
-      to: '<EMAIL_USER>'
-      html: '{{ template "email.default.html" . }}'
-      headers: { Subject: " {{ .CommonAnnotations.summary }} " }
+            sed "s@<WEBHOOK_URL>@$WEBHOOK_URL@g" | \
 
-  - name: 'slack'
-    slack_configs:
-      - send_resolved: true
-        text: "{{ .CommonAnnotations.description }}"
-        username: '<SLACK_USER>'
-        channel: '<SLACK_CHANNEL>'
-        api_url: '<SLACK_URL>'
+            sed "s@<REPEAT>@$REPEAT@g" > /tmp/alertmanager.yml
 
-  - name: 'webhook'
-    webhook_configs:
-      - url: '<WEBHOOK_URL>'
-        send_resolved: true
+        mv /tmp/alertmanager.yml /etc/alertmanager/config/alertmanager.yml
+    fi
+
+    set -- /bin/alertmanager "$@"
+fi
+
+exec "$@"
